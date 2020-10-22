f69e04badf303af2512a7c80bed4bbd8918525d9
Wojciech-Sak89 <wojciech.r.sak@gmail.com>
2020-10-22 12:42:45 +0200

    Final refactor v1.2

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
   var apiRoot = 'https://secure-escarpment-67159.herokuapp.com/v1/';
+  const trelloApiRoot = 'https://secure-escarpment-67159.herokuapp.com/v1/trello/';
   // const apiRoot = 'http://localhost:8080/v1/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  // const trelloApiRoot = 'http://localhost:8080/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
